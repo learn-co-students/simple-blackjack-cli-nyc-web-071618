dadcec87ce9d87b3fad22fa41b91bc0baf13e939
William Gao <williamgao70@yahoo.com>
2018-07-12 07:17:50 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,10 +1,8 @@
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
   card = rand(1..10)
   
   if card == 1
@@ -52,19 +50,13 @@ def get_card_total(cards)
 end
 
 def display_cards_and_total(cards)
-  # code #display_cards here
   card_total = get_card_total(cards)
   puts "Cards: #{cards}"
   puts "Total: #{card_total}"
 end
 
-def prompt_user
-  # code #prompt_user here
-  puts "Type 'h' to hit or 's' to stay"
-end
-
 def get_user_input
-  # code #get_user_input here
+  puts "Type 'h' to hit or 's' to stay"
   gets.chomp
 end
 
@@ -76,16 +68,32 @@ end
 
 def initial_round
   # code #initial_round here
-  card1 = deal_card
-  card2 = deal_card
-  return [card1, card2]
+  player_card_1 = deal_card
+  player_card_2 = deal_card
+  
+  dealer_card_1 = deal_card
+  dealer_card_2 = deal_card
+  return [player_card_1, player_card_2], [dealer_card_1, dealer_card_2]
 end
 
-def hit?(cards)
-  # code hit? here
-  prompt_user
-  input = get_user_input
+def hit?(cards, is_dealer=false)
   
+  # dealer - Crude AI
+  if(is_dealer == true)
+    cards_total = get_card_total(cards)
+    randnum = rand(100)%2
+    if(card_total == 21 || randnum == 0)
+      return cards
+    elsif(randnum == 1)
+      new_card = deal_card
+      cards.push(new_card)
+    end
+    return cards
+  end
+  
+  # player
+  input = get_user_input
+
   if(input == "s")
     return cards
   end
@@ -93,6 +101,7 @@ def hit?(cards)
   if(input == "h")
     new_card = deal_card
     cards.push(new_card)
+    return cards
   else
     invalid_command
     hit?(cards)
@@ -111,14 +120,48 @@ end
 def runner
   # code runner here
   welcome
-  cards = initial_round
+  player_cards, dealer_cards = initial_round
+  display_cards_and_total(player_cards)
+  player_card_total = get_card_total(player_cards)
+   dealer_cards_total = get_card_total(dealer_cards)
+  player_stay = false
+  dealer_stay = false
+  player_busted = false
+  dealer_busted = false
   
-  loop do
-    card_total = get_card_total(cards)
-    display_cards_and_total(cards)
-    break if card_total > 21
-    cards = hit?(cards)
+  while !player_stay|| !dealer_stay
+    player_cards_temp = hit?(player_cards)
+    
+    if(!dealer_busted)
+      dealer_cards_temp = hit?(dealer_cards, true)
+    end
+    
+    player_cards_temp == player_cards ? player_stay = true : player_stay = false
+    dealer_cards_temp == dealer_cards ? dealer_stay = true : dealer_stay = false
+    
+    player_cards = player_cards_temp
+    dealer_cards = dealer_cards_temp
+    display_cards_and_total(player_cards)
+    display_cards_and_total(dealer_cards)
+    
+    player_card_total = get_card_total(player_cards)
+    
+    if player_card_total > 21
+      player_busted = true
+      break
+    end
+    
+    dealer_cards_total = get_card_total(dealer_cards)
+    
+    if dealer_cards_total > 21 
+      dealer_busted = true
+    end
   end
   
-  end_game(cards)
+  if(player_busted)
+    end_game(player_cards)
+  elsif(dealer_busted)
+    puts "You Win!"
+    
+  end
 end
