43bd67a619226692d7edb21906061d3044d42515
dagonyclark <sean@dagony-clark.com>
2019-11-15 17:00:17 -0500

    Revert "Done."
    This reverts commit 6f9bb5ebfcccb089eb9b7cb87a62b8df01d7a7d4.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,62 +1,37 @@
-require "pry"
-
 def welcome
-  puts "Welcome to the Blackjack Table"# code #welcome here
-  return "Welcome to the Blackjack Table"
+  # code #welcome here
 end
 
 def deal_card
-  return rand(10)+1# code #deal_card here
+  # code #deal_card here
 end
 
-def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"# code #display_card_total here
-  return card_total
+def display_card_total
+  # code #display_card_total here
 end
 
 def prompt_user
-  puts "Type 'h' to hit or 's' to stay"# code #prompt_user here
+  # code #prompt_user here
 end
 
 def get_user_input
-  gets.chomp# code #get_user_input here
+  # code #get_user_input here
 end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"# code #end_game here
+def end_game
+  # code #end_game here
 end
 
 def initial_round
-  card_total = 0
-  2.times do
-    card_total += deal_card# code #initial_round here
-  end
-  display_card_total(card_total)
-  return card_total
+  # code #initial_round here
 end
 
-def hit?(card_total)
-  prompt_user
-  answer = get_user_input
-  if answer == "h"
-    # binding.pry
-    card_total += deal_card
-    # display_card_total(card_total)
-    # puts "Your hand equals #{card_total}"
-  elsif answer == "s"
-    # display_card_total(card_total)
-    # puts "Your hand equals #{card_total}"
-  else
-    invalid_command
-  end
-  return card_total
+def hit?
+  # code hit? here
 end
-
-# hit?(rand(11))
 
 def invalid_command
-  puts "Please enter a valid command"
-  prompt_user
+  # code invalid_command here
 end
 
 #####################################################
@@ -64,15 +39,6 @@ end
 #####################################################
 
 def runner
-  welcome
-  card_total = initial_round
-  # hit?(initial_round)
-  until card_total > 21 do
-    # binding.pry
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
+  # code runner here
 end
-
-# runner
+    
